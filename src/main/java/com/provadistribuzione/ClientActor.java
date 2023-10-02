cacbbcb8f9b2f1b1c56e290de5d7b7baf17c0347
Kevin <k.mazzoni@reply.it>
2023-10-02 15:50:37 +0200

    ClientActor e ServerActor comunicano. Il client manda un messaggio che viene correttamente visualizzato dal server

--- src/main/java/com/provadistribuzione/ClientActor.java
+++ src/main/java/com/provadistribuzione/ClientActor.java
@@ -1,16 +1,25 @@
+//File ClientActor.java
 package com.provadistribuzione;
 
 import akka.actor.AbstractActor;
 import akka.actor.ActorRef;
+import akka.actor.ActorSelection;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import akka.pattern.Patterns;
+import akka.util.Timeout;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
+import scala.concurrent.Await;
+import scala.concurrent.Future;
+import scala.concurrent.duration.Duration;
+
+import java.util.concurrent.TimeUnit;
 
 public class ClientActor extends AbstractActor {
-    private final ActorRef serverActor;
+    private final ActorSelection serverActor;
 
-    public ClientActor(ActorRef serverActor) {
+    public ClientActor(ActorSelection serverActor) {
         this.serverActor = serverActor;
     }
 
@@ -24,23 +33,34 @@ public class ClientActor extends AbstractActor {
     }
 
     public static void main(String[] args) {
-        Config config = ConfigFactory.load(/*"client.conf"*/);
+        Config config = ConfigFactory.load();
         ActorSystem system = ActorSystem.create("ClientSystem", config);
 
         System.out.println("PORTAAAAAAA: " + System.getenv("PORT"));
 
         // Crea un riferimento all'attore del server
-        // ActorRef serverActor = system.actorSelection("akka://Server@192.168.132.1:25520/user/ServerActor").anchor();
-        ActorRef serverActor = system.actorSelection("akka://Server@127.0.0.1:2551/user/ServerActor").anchor();
+        ActorSelection serverActor = system.actorSelection("akka://ServerSystem@127.0.0.1:2551/user/ServerActor");
 
         // Crea e avvia l'attore del client, passando il riferimento all'attore del server
-        ActorRef clientActor = system.actorOf(Props.create(ClientActor.class, serverActor), "clientActor");
+        system.actorOf(ClientActor.props(serverActor), "clientActor");
 
         // Invia un messaggio al server
-        clientActor.tell("inviaMessaggio", clientActor);
+        serverActor.tell("inviaMessaggio", ActorRef.noSender());
 
-        //Provo ad inviare un messaggio al server in un altro modo
-        serverActor.tell("Provamessaggio" , clientActor);
+        // Prova a inviare un messaggio al server in un altro modo
+        try {
+            Timeout timeout = Timeout.durationToTimeout(Duration.create(5, TimeUnit.SECONDS));
+            Future<Object> future = Patterns.ask(serverActor, "Provamessaggio", timeout);
+            String result = (String) Await.result(future, timeout.duration());
+            System.out.println("Risposta dal server: " + result);
+        } catch (Exception e) {
+            System.out.println("Errore durante l'attesa della risposta dal server: " + e.getMessage());
+        } finally {
+            system.terminate();
+        }
+    }
 
+    public static Props props(ActorSelection serverActor) {
+        return Props.create(ClientActor.class, serverActor);
     }
 }
--- src/main/java/com/provadistribuzione/ServerActor.java
+++ src/main/java/com/provadistribuzione/ServerActor.java
@@ -1,20 +1,18 @@
 package com.provadistribuzione;
 
 import akka.actor.AbstractActor;
-import akka.actor.ActorSelection;
+import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
-
-import java.io.File;
-import scala.concurrent.duration.Duration;
-import java.util.concurrent.TimeUnit;
-
-import com.environment.HVACActor;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
+import scala.concurrent.duration.Duration;
+
+import java.util.concurrent.TimeUnit;
 
 public class ServerActor extends AbstractActor {
-    // public static final String stringConfig = "akka { actor { provider = 'akka.remote.RemoteActorRefProvider'} remote {enabled-transports = ['akka.remote.netty.tcp'] netty.tcp {hostname = '127.0.0.1' port = 6123}}}";
+
+    private static boolean isClientRunning = false;
 
     @Override
     public Receive createReceive() {
@@ -22,102 +20,37 @@ public class ServerActor extends AbstractActor {
                 .match(String.class, message -> {
                     System.out.println("Server ha ricevuto il messaggio: " + message);
                 })
+                .matchEquals("ClientStarted", message -> {
+                    isClientRunning = true;
+                    System.out.println("Il client è stato avviato.");
+                })
                 .build();
-
-        // return receiveBuilder()
-        //         .matchAny(message -> {
-        //             if (message instanceof String) {
-        //                 String content = (String) message;
-        //                 System.out.println("Server ha ricevuto il messaggio: " + content);
-        //             } else {
-        //                 System.out.println("UNHANDLEDDDDDDDDDDDDDDDD!!!!!!");
-        //                 unhandled(message);
-        //             }
-        //         })
-        //         .build();
-
     }
 
     public static void main(String[] args) {
-        
-        //Qui valido
-        System.out.println("Il ServerActor sta eseguendo il main");
-        System.out.println("PORTAAAAAAA: " + System.getenv("PORT"));
-        
-        
-        //Qui non valido
-        // Config config = ConfigFactory.load("application.conf");
-        // File conf = new File("application.conf");
-        // Config config = ConfigFactory.parseFile(conf);
-        // Config config = ConfigFactory.load();
-        // Config config = ConfigFactory.load("kghgchchjfc");
-        // String configString = config.getString("application.conf");
-        // boolean isResolved = config.isResolved();
-        // boolean fileIsPresent = config.hasPath("./provadistribuzione");
-        // System.out.println("File is present at that path: " + " ConfigString is: " + " isResolved: " + isResolved);
-        // Config config = ConfigFactory.parseFile(new File("./application.conf"));
-        // File applicationFile = new File("src/main/java/com/provadistribuzione/application.conf");
-        // Config config = ConfigFactory.parseFile(applicationFile);
-        // System.out.println("L'application.conf file è: " + applicationFile.getName() + " il path è: " + applicationFile.getAbsolutePath());
-        
-        //Qui valido
+        // Carica la configurazione da application.conf
         Config config = ConfigFactory.load();
-        ActorSystem system = ActorSystem.create("Server", config);
-        system.actorOf(ServerActor.props(), "ServerActor");
+
+        // Crea un sistema degli attori
+        ActorSystem system = ActorSystem.create("ServerSystem", config);
+
+        // Crea l'attore del server
+        // ServerActor serverActor = new ServerActor();
+        ActorRef serverActor = system.actorOf(Props.create(ServerActor.class), "ServerActor");
+
+        // Attendi che il client venga avviato (puoi sostituire questo con la tua logica)
+        while (!isClientRunning) {
+            try {
+                Thread.sleep(1000); // Attendi 1 secondo
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+
         System.out.println("Il ServerActor ha terminato di eseguire il main");
-
-        //Una prova
-        // System.out.println("Il ServerActor sta eseguendo il main");
-
-        // System.out.println("PORTAAAAAAA: " + System.getenv("PORT"));
-        // Config config = ConfigFactory.load();
-        // ActorSystem system = ActorSystem.create("Server", config);
-        // system.actorOf(ServerActor.props(), "ServerActor");
-
-        // // Tentativo di connessione al client
-        // ActorSelection clientSelection = system.actorSelection("akka://ClientSystem@127.0.0.1:2552/user/ClientActor");
-
-        // int maxRetries = 10; // Numero massimo di tentativi
-        // int retries = 0;
-        // boolean connected = false;
-
-        // while (retries < maxRetries && !connected) {
-        //     try {
-        //         clientSelection.resolveOne(Duration.create(5, TimeUnit.SECONDS)).toCompletableFuture().get();
-        //         connected = true;
-        //         System.out.println("Connessione al client riuscita!");
-        //     } catch (Exception e) {
-        //         retries++;
-        //         System.out.println("Tentativo di connessione al client #" + retries + " fallito. Riproverà tra 5 secondi...");
-        //         try {
-        //             Thread.sleep(5000); // Attendere per 5 secondi prima di un nuovo tentativo
-        //         } catch (InterruptedException ex) {
-        //             Thread.currentThread().interrupt();
-        //         }
-        //     }
-        // }
-
-        // if (!connected) {
-        //     System.out.println("Impossibile connettersi al client dopo " + maxRetries + " tentativi. Uscita.");
-        //     system.terminate();
-        //     return;
-        // }
-
-        // System.out.println("Il ServerActor ha terminato di eseguire il main");
-
-        //Un'altra prova
-        // System.out.println("Il ServerActor sta eseguendo il main");
-
-        // System.out.println("PORTAAAAAAA: " + System.getenv("PORT"));
-        // Config config = ConfigFactory.load();
-        // ActorSystem system = ActorSystem.create("Server", config);
-        // system.actorOf(ServerActor.props(), "ServerActor");
-
-        // System.out.println("Il ServerActor ha terminato di eseguire il main");
-
     }
 
     static Props props() {
-		return Props.create(ServerActor.class);
-	}
+        return Props.create(ServerActor.class);
+    }
 }
